ec3017ba69da8b3720980b14581b684fda20596f
dintees <41068795+dintees@users.noreply.github.com>
2021-06-27 17:40:02 +0200

    Created a basic project structure, instalded modules

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 var http = require("http");
-var server = http.createServer(function(req,res){
+var fs = require("fs");
+
+var server = http.createServer(function (req, res) {
     // parametr res oznacza obiekt odpowiedzi serwera (response)
     // parametr req oznacza obiekt żądania klienta (request)
+
+    if (req.url == "/") {
+        fs.readFile("static/index.html", function (error, data) {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(data);
+        })
+    } else if (req.url.indexOf("css") != -1) {
+        fs.readFile("static" + req.url, function (error, data) {
+            res.writeHead(200, { 'Content-Type': 'text/css' });
+            res.end(data);
+        })
+    }
+    else if (req.url.indexOf("js") != -1) {
+        fs.readFile("static" + req.url, function (error, data) {
+            res.writeHead(200, { 'Content-Type': 'application/js' });
+            res.end(data);
+        })
+    }
 })
 
-server.listen(3000, function(){
-   console.log("serwer startuje na porcie 3000")
+server.listen(3000, function () {
+    console.log("start serwera na porcie 3000")
 });
