20aa24a9ac79264088994bcb5213140fdbd7e184
KP <kplis@student.agh.edu.pl>
2021-07-08 12:12:35 +0200

    mudule division

--- server/socket.js
+++ server/socket.js
@@ -1,31 +1,68 @@
 var socket = require("socket.io");
 
-function socket_connection(socket) {
-    console.log("The client has connected!");
-    socket.on("login", socket_login)
-    socket.on("disconnect", socket_disconnect)
+var data = new Map();
 
-    // socket.on("event", data => {
-    // console.log(data)
-    // })
+module.exports = class Socket {
+    static io
+    constructor(server) {
+        init(server)
+    }
+
+    static init(server)
+    {
+        var io = socket(server)
+        io.on("connection", Socket.connection)
+
+        Socket.io = io
+    }
+    static connection(socket) {
+        console.log("The client has connected!");
+        socket.on("login", Socket.login(socket))
+        socket.on("disconnect", Socket.disconnect)
+    }
+
+    static login(socket) {
+        return function(data)
+        {
+            console.log(data)
+            // czy taki użytkownik ustnieje
+    
+            // req.session
+            socket.emit("login", { success: true });
+            // socket.emit("login", {success: false, comment: "Bad username or password"});
+        }
+    }
+
+    static disconnect() {
+        console.log("The client has disconnected!");
+    }
 }
-function socket_login(data) {
-    console.log(data)
-    // czy taki użytkownik ustnieje
+// io = socket(server);
 
-    // req.session
-    socket.emit("login", { success: true });
-    // socket.emit("login", {success: false, comment: "Bad username or password"});
-}
+// io.on("connection", function (socket) {
+//     console.log("The client has connected!");
+//     data.set(socket.id,{test:""})
 
-function socket_disconnect() {
-    console.log("The client has disconnected!");
-}
+//     var dd = data.get(socket.id)
+//     dd.test += "1"
+//     console.log(data.get(socket.id))
+//     socket.on("login", function (data) {
+//         console.log(data)
+//         // czy taki użytkownik ustnieje
+
+//         // req.session
+//         socket.emit("login", { success: true });
+//         // socket.emit("login", {success: false, comment: "Bad username or password"});
+//     })
+//     socket.on("disconnect", function () {
+//         console.log("The client has disconnected!");
+//         data.delete(socket.id)
+//     })
+
+//     // socket.on("event", data => {
+//     // console.log(data)
+//     // })
+// })
+///io.on("connection", soc.socket_connection)
 
 
-
-
-
-exports.socket_connection = socket_connection
-exports.socket_login = socket_login
-exports.socket_disconnect = socket_disconnect
