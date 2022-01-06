7123819b90d47c35303e731fe8fc7c4153247c13
D1ZZyyy <krzysztofszarek2000@gmail.com>
2022-01-06 21:44:15 +0100

    login form

--- book/src/app/pages/login/login.component.ts
+++ book/src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+
+  hide = true;
+
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'Musisz podać email';
+    }
+
+    return this.email.hasError('email') ? 'Niepoprawny email' : '';
+  }
+
 
   constructor() { }
 
