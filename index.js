6a888efe1ec042792e995c79eee01c9e7bf0fe1b
dintees <41068795+dintees@users.noreply.github.com>
2022-02-13 14:41:00 +0100

    Added post functions, nedb database

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
+// Import modules
 const express = require('express');
 const path = require('path')
 const bodyParser = require('body-parser');
 const port = 3000;
 const app = express();
+const Datastore = require('nedb');
 
+// File modules
+var Database = require('./modules/Database');
+
+// Databases
+var users = new Datastore({ filename: "db/users.db", autoload: true });
+
+// Load Angular files
 app.use(express.static(path.join(__dirname, './book/dist/book')))
 
-app.use(bodyParser.urlencoded({extended: true}));
+// Use POST method
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.post('/query', (req, res) => {
-    console.log(req.body)
-    let obj = { action: req.body.action, success: true, message: "OK" }
+app.post('/api/query', (req, res) => {
+    console.log(req.body);
+    let obj = {};
+    switch (req.body.action) {
+        case "login":
+            console.log("Login action");
+            break;
+        case "register":
+            console.log("Register action");
+            break;
+
+        default:
+            obj = { success: false, message: "Unknown command" }
+    }
     res.send(obj)
 })
 
---
+++ modules/Database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    // Insert
+    Insert: function(coll, data, callback) {
+        // coll.insert(data, (err, doc) => callback(err, doc))
+        console.log("Insert function")
+    },
+
+    Select: function(coll, data) {
+        // db.insert(data)
+        console.log("Insert function")
+    }
+}
