34a89491ead5510d1dd45f6da4556304e555dc69
dintees <41068795+dintees@users.noreply.github.com>
2022-02-13 10:30:21 +0100

    Added bodyParser and post functionalities

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const path = require('path')
+const bodyParser = require('body-parser');
 const port = 3000;
 const app = express();
 
 app.use(express.static(path.join(__dirname, './book/dist/book')))
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
-app.get("/query", (req,res) => {
-    var obj = { action: "TEST", success: true, message: "OK" }
-    res.send(JSON.parse(obj))
+app.post('/query', (req, res) => {
+    console.log(req.body)
+    let obj = {
+        action: req.body.action,
+        success: true,
+        message: "OK"
+    }
+    res.send(obj)
 })
 
 
