2fa81f23ad651d7128f76070ab703228ef69d8ea
dintees <41068795+dintees@users.noreply.github.com>
2022-02-14 09:24:19 +0100

    Added login and register action with the use of the database to the server

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const Datastore = require('nedb');
 
 // File modules
 var Database = require('./modules/Database');
+const { redirect } = require('express/lib/response');
 
 // Databases
 var users = new Datastore({ filename: "db/users.db", autoload: true });
@@ -25,15 +26,43 @@ app.post('/api/query', (req, res) => {
     switch (req.body.action) {
         case "login":
             console.log("Login action");
+            if (req.body.login == "" && req.body.password == "") res.send({ action: "login", success: false, message: "Empty login or password" });
+            else {
+                Database.SelectOne(users, { login: req.body.login, password: req.body.password }, (err, data) => {
+                    console.log(data);
+                    if (data) { delete data.password; res.send({ action: "login", success: true, user: data }) }
+                    else res.send({ action: "login", success: false, message: "Incorrect login or password." })
+                })
+            }
             break;
+
         case "register":
             console.log("Register action");
+            if (req.body.password == req.body.password2) {
+                Database.SelectOne(users, { login: req.body.login }, (err, doc) => {
+                    if (doc) res.send({ action: "register", success: false, message: "There is a user with the same login." })
+                    else {
+                        Database.SelectOne(users, { email: req.body.email }, (err, doc) => {
+                            if (doc) res.send({ action: "register", success: false, message: "There is a user with the same email." })
+                            else {
+                                let user = { login: req.body.login, password: req.body.password, email: req.body.email }
+                                Database.Insert(users, user, (err, newDoc) => {
+                                    if (newDoc) { delete newDoc.password; res.send({ action: "register", success: true, user: newDoc }) }
+                                    else res.send({ action: "register", success: false, message: "Error while adding new user." })
+                                })
+                            }
+                        })
+                    }
+                })
+            } else {
+                res.send({ action: "register", success: false, message: "The passwords are not identical." })
+            }
             break;
 
         default:
             obj = { success: false, message: "Unknown command" }
+            res.send(obj)
     }
-    res.send(obj)
 })
 
 
