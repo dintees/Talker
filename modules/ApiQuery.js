2d4d7854976c9c426bb37de2432ccb741c9c38d9
dintees <41068795+dintees@users.noreply.github.com>
2022-04-05 17:43:00 +0200

    Installed socket.io, improwed server functionalities, splitted the server into files

---
+++ modules/ApiQuery.js
@@ -0,0 +1,41 @@
+var Database = require('./Database')
+module.exports = {
+    Login: function (req, users) {
+        return new Promise(resolve => {
+            console.log(" - LOGIN - ");
+            if (req.body.login == "" && req.body.password == "") resolve({ action: "login", success: false, message: "Empty login or password" });
+            else {
+                Database.SelectOne(users, { login: req.body.login, password: req.body.password }, (err, data) => {
+                    // console.log(data);
+                    if (data) { delete data.password; resolve({ action: "login", success: true, user: data }) }
+                    else resolve({ action: "login", success: false, message: "Incorrect login or password" })
+                })
+            }
+        })
+    },
+
+    Register: function(req, users) {
+        console.log(" - REGISTER - ");
+        return new Promise(resolve => {
+            if (req.body.password == req.body.password2) {
+                Database.SelectOne(users, { login: req.body.login }, (err, doc) => {
+                    if (doc) resolve({ action: "register", success: false, message: "There is a user with the same login" })
+                    else {
+                        Database.SelectOne(users, { email: req.body.email }, (err, doc) => {
+                            if (doc) resolve({ action: "register", success: false, message: "There is a user with the same email" })
+                            else {
+                                let user = { login: req.body.login, password: req.body.password, email: req.body.email }
+                                Database.Insert(users, user, (err, newDoc) => {
+                                    if (newDoc) { delete newDoc.password; resolve({ action: "register", success: true, user: newDoc }) }
+                                    else resolve({ action: "register", success: false, message: "Error while adding new user" })
+                                })
+                            }
+                        })
+                    }
+                })
+            } else {
+                resolve({ action: "register", success: false, message: "The passwords are not identical" })
+            }
+        })
+    }
+}
